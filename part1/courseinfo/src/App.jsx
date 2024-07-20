b3661425c7b00c87ac7c027070c79c0e7e89c099
jamessenior642 <jvsenior02@gmail.com>
2024-07-19 20:20:15 -0400

    finished 1.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,31 @@
+const Header = ({ name }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+    </div>
+  )
+}
+
+const Content = ({ part, count }) => {
+  return (
+    <div>
+      <p>
+        {part} {count}
+      </p>
+    </div>
+  )
+}
+
+const Total = ({ one, two, three }) => {
+  return (
+    <div>
+      <p>
+        Number of exercises {one + two + three}
+      </p>
+    </div>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -7,20 +35,13 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
-  
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header name={course} />
+      <Content part={part1} count={exercises1} />
+      <Content part={part2} count={exercises2} />
+      <Content part={part3} count={exercises3} />
+      <Total one={exercises1} two={exercises2} three={exercises3} />
     </div>
   )
 }
