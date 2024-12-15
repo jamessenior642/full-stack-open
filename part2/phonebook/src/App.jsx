0ccb2b6bbdd801b8d97ca16e1838087fc014a4d6
jamessenior642 <jvsenior02@gmail.com>
2024-12-15 18:40:19 -0500

    Finished 2.7 - no duplicate names

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,19 +6,37 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat(personObject))
+      setNewName('')
+    }
+  
+  }
+
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handlePersonChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => <div key={person.name}>{person.name}</div>)}
     </div>
   )
 }
