b3f0a62739728e7a09060ece040c5eb550ae4f5e
jamessenior642 <jvsenior02@gmail.com>
2024-12-18 18:30:09 -0500

    Finished 2.19 - added show button

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   const [search, setSearch] = useState('')
   const [countries, setCountries] = useState([])
   const [filteredCountries, setFilteredCountries] = useState([])
+  const [selectedCountry, setSelectedCountry] = useState(null);
 
   useEffect(() => {
     countryService.getAll().then(data => {
@@ -16,7 +17,7 @@ function App() {
   const handleSearchChange = (event) => {
     const query = event.target.value.toLowerCase()
     setSearch(query)
-    console.log(query)
+    setSelectedCountry(null);
     if (query === '') {
       setFilteredCountries([])
     } else {
@@ -25,15 +26,24 @@ function App() {
       setFilteredCountries(filtered)
     }
   }
+
+  const handleShowCountry = (country) => {
+    setSelectedCountry(country); // Set the selected country for detailed view
+  };
   
   let countryList
-  if (filteredCountries.length === 0) {
+  if (selectedCountry) {
+    countryList = <CountryDisplay country={selectedCountry} />;
+  } else if (filteredCountries.length === 0) {
     countryList === null
   } else if (filteredCountries.length === 1) {
     countryList = <CountryDisplay country={filteredCountries[0]}/>
   } else if (filteredCountries.length <= 10) {
     countryList = filteredCountries.map(c => 
-    <p key={c.name.common}>{c.name.common}</p>)
+    <p key={c.name.common}>
+      {c.name.common}
+      <button onClick={() => handleShowCountry(c)}>Show</button>
+    </p>)
   } else {
     countryList = <p>Too many matches, specify another filter</p>
   }
