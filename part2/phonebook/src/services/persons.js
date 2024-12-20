e1030c7d7d3fd88ba9346334045d7f0205b38b4d
jamessenior642 <jvsenior02@gmail.com>
2024-12-20 15:12:55 -0500

    3.10 - connected to render app

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3003/api/persons'
+// const localUrl = 'http://localhost:3003/api/persons'
+const remoteUrl = 'https://fso-server.onrender.com/api/persons'
+const baseUrl = remoteUrl
 
 const getAll = () => {
     const request = axios.get(baseUrl)
